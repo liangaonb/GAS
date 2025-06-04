c94ead56b9df3aa3f225ed6f0d78fc314ab42148
Ciaccona <1586906937@qq.com>
2025-06-05 00:25:50 +0800

    add player controller

--- Source/GAS/Public/Player/AuraPlayerController.h
+++ Source/GAS/Public/Player/AuraPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
 /**
  * 
  */
@@ -17,4 +20,13 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
