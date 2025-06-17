480e2cbb1224d1a2f18e64d17d20f557449c218a
Ciaccona <1586906937@qq.com>
2025-06-18 00:29:27 +0800

    add callbacks for status change

--- Source/GAS/Private/UI/HUD/AuraHUD.cpp
+++ Source/GAS/Private/UI/HUD/AuraHUD.cpp
@@ -27,6 +27,7 @@ UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetCont
 	{
 		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
 		OverlayWidgetController->SetWidgetControllerParams(WCParams);
+		OverlayWidgetController->BindCallbacksToDependencies();
 
 		return OverlayWidgetController;
 	}
